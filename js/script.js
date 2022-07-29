484a4eaf0b77e618fd43f5a4d7ba0e8342e760ed
Ira <mamlin.ira@gmail.com>
2022-07-29 12:37:59 +0300

    create object personalMovieDB

--- js/script.js
+++ js/script.js
@@ -1,6 +1,25 @@
 "use strict";
 
-let a = 10;
+// Lesson 19 
 
-console.log(a++);
-console.log(a);
+const numberOfFilms = +prompt('numberOfFilms?', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false,
+};
+
+const lastFilm1 = prompt('Last film'),
+      ratingFilm1 = prompt('Rating film'),
+      lastFilm2 = prompt('Last film'),
+      ratingFilm2 = prompt('Rating film');
+
+personalMovieDB.movies = {
+    [lastFilm1]: ratingFilm1,
+    [lastFilm2]: ratingFilm2
+};
+
+console.log(personalMovieDB);
